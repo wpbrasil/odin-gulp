25eb06e5ff5ffbcd34f89a8f7660b637d3cf83bf
Fernando Daciuk <f.daciuk@gmail.com>
2014-02-08 22:51:37 -0200

    Uglify task

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var ssh       = require( 'gulp-ssh' );
 var uglify    = require( 'gulp-uglify' );
 var zip       = require( 'gulp-zip' );
 
+
+
 var dirs = {
 	js: '../assets/js',
 	sass: '../assets/sass',
@@ -19,18 +21,79 @@ var dirs = {
 };
 
 
+
 gulp.task( 'jshint', function() {
 	gulp.src( dirs.js )
-		.pipe( jshint() );
+	.pipe( jshint() )
+	.pipe( jshint.reporter( 'default' ) );
 });
+
+
+
+gulp.task( 'uglify', function() {
+	gulp.src([
+		'<%= dirs.js %>/libs/*.js', // External libs/plugins
+		'<%= dirs.js %>/main.js'    // Custom JavaScript
+	])
+	.pipe( uglify() )
+	.pipe( gulp.dest( dirs.js + '/main.min.js' ) );
+});
+
+
+
+gulp.task( 'uglify-bootstrap', function() {
+	gulp.src([
+		dirs.js + '/bootstrap/transition.js',
+		dirs.js + '/bootstrap/alert.js',
+		dirs.js + '/bootstrap/button.js',
+		dirs.js + '/bootstrap/carousel.js',
+		dirs.js + '/bootstrap/collapse.js',
+		dirs.js + '/bootstrap/dropdown.js',
+		dirs.js + '/bootstrap/modal.js',
+		dirs.js + '/bootstrap/tooltip.js',
+		dirs.js + '/bootstrap/popover.js',
+		dirs.js + '/bootstrap/scrollspy.js',
+		dirs.js + '/bootstrap/tab.js',
+		dirs.js + '/bootstrap/affix.js'
+	])
+	.pipe( uglify() )
+	.pipe( gulp.dest( dirs.js + '/libs/bootstrap.min.js' ) );
+});
+
+
 
 gulp.task( 'compass', function() {
 
 });
 
-gulp.task( 'uglify', function() {
-
-});
 
 
+/**
+ * Execution Tasks
+ */
 gulp.task( 'default', [ 'jshint', 'compass', 'uglify' ] );
+gulp.task( 'optimize', [ 'imagemin' ] );
+gulp.task( 'ftp', [ 'ftp-deploy' ] );
+gulp.task( 'compress', [ 'default', 'zip' ] );
+gulp.task( 'bootstrap', [
+	'clean-prepare',
+	'curl-bootstrap-sass',
+	'unzip-bootstrap-scss',
+	'rename-bootstrap-scss',
+	'rename-bootstrap-js',
+	'rename-bootstrap-fonts',
+	'clean-bootstrap',
+	'uglify-bootstrap',
+	'compass'
+]);
+
+
+
+/**
+ * Short aliases
+ */
+gulp.task( 'w', [ 'watch' ] );
+gulp.task( 'o', [ 'optimize' ] );
+gulp.task( 'f', [ 'ftp' ] );
+gulp.task( 'r', [ 'rsync' ] );
+gulp.task( 'c', [ 'compress' ] );
